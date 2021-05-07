f7bdaa8eecc6239b2a235418e2f0d5340b03d4bf
FrancescaLai <francescalai1690@gmail.com>
2021-05-07 17:19:19 +0200

    Static header done and static main wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    
+    {{-- Google Font --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    
+    {{-- css --}}
     <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
     <title>La Molisana</title>
 </head>
@@ -12,10 +17,22 @@
 
     <header>
         <div class="container">
-            <img src="{{ asset('img/molisana-logo.png') }}" alt="">
-
+            <div class="logo">
+                <img src="{{ asset('img/molisana-logo.png') }}" alt="">
+            </div>
+            <nav class="navbar">
+                <ul>
+                    <li class="active"> <a href="">Home</a></li>
+                    <li> <a href="">Prodotti</a></li>
+                    <li><a href="">News</a></li>
+                </ul>
+            </nav>
         </div>
     </header>
+
+    <main>
+        
+    </main>
     
 </body>
 </html>
